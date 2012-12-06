6dd447764aee730084362b999da85045d6dc21ca
Salil Wadnerkar <rohshall@gmail.com>
2012-12-06 22:48:30 +0800

    reads the hacker news home page

---
+++ hnews.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from urllib.request import urlopen
+import json
+
+feed = urlopen("http://hndroidapi.appspot.com/news/format/json/page/").read()
+print(type(feed))
+articles_map = json.loads(feed.decode("utf-8"))
+print(type(articles_map))
+articles = articles_map["items"]
+for article in articles:
+  if article.get("item_id"):
+    print(article["item_id"] + ": " + article["title"])
+    if article.get("url"):
+      print(article["url"])
+    print()
+
+
